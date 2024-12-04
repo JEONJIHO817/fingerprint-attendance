42c8006a434cc877385c8f22e3759934ec24bed8
unknown <jeonjiho817@jbnu.ac.kr>
2024-12-05 06:21:57 +0900

    signin

--- js/student.js
+++ js/student.js
@@ -11,29 +11,29 @@ WildRydes.map = WildRydes.map || {};
         if (token) {
             authToken = token;
         } else {
-            window.location.href = '/signin.html';
+            window.location.href = './signin.html';
         }
     }).catch(function handleTokenError(error) {
         alert(error);
-        window.location.href = '/signin.html';
+        window.location.href = './signin.html';
     });
 
     $(function onDocReady() {
         // 출근부 조회 버튼 클릭 시
         $('#checkAttendance').click(function () {
-            window.location.href = '/attendance.html'; // 출근부 조회 페이지로 이동
+            window.location.href = './attendance.html'; // 출근부 조회 페이지로 이동
         });
 
         // 출/퇴근하기 버튼 클릭 시
         $('#clockInOut').click(function () {
-            window.location.href = '/clockInOut.html'; // 출/퇴근 페이지로 이동
+            window.location.href = './clockInOut.html'; // 출/퇴근 페이지로 이동
         });
 
         // 로그아웃 버튼 클릭 시
         $('#signOut').click(function () {
             WildRydes.signOut();
             alert('로그아웃되었습니다.');
-            window.location.href = '/signin.html';
+            window.location.href = './signin.html';
         });
     });
 }(jQuery));
