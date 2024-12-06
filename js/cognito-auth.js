38be1104be550146a40253c5e4bb363e1cfda015
unknown <jeonjiho817@jbnu.ac.kr>
2024-12-07 02:12:49 +0900

    student

--- js/cognito-auth.js
+++ js/cognito-auth.js
@@ -4,6 +4,7 @@ var WildRydes = window.WildRydes || {};
 
 (function scopeWrapper($) {
     var signinUrl = '/signin.html';
+    var studentUrl = '/student.html'; // 로그인 상태일 경우 이동할 URL
 
     var poolData = {
         UserPoolId: _config.cognito.userPoolId,
@@ -25,13 +26,32 @@ var WildRydes = window.WildRydes || {};
         AWSCognito.config.region = _config.cognito.region;
     }
 
-    // 로그아웃 기능 수정
+    // 세션 확인 및 자동 리다이렉션
+    $(document).ready(function () {
+        var cognitoUser = userPool.getCurrentUser();
+
+        if (cognitoUser) {
+            cognitoUser.getSession(function (err, session) {
+                if (err || !session.isValid()) {
+                    console.log('Invalid or expired session.');
+                    sessionStorage.removeItem('authToken');
+                    return; // 세션이 없거나 만료된 경우 아무 동작도 하지 않음
+                }
+
+                // 세션이 유효한 경우
+                console.log('User is already signed in. Redirecting...');
+                window.location.href = studentUrl; // student.html로 이동
+            });
+        } else {
+            console.log('No user is signed in.');
+        }
+    });
+
     WildRydes.signOut = function signOut() {
         userPool.getCurrentUser().signOut();
         sessionStorage.removeItem('authToken'); // 세션 스토리지에서 토큰 제거
     };
 
-    // authToken을 세션 스토리지에서 관리
     WildRydes.authToken = new Promise(function fetchCurrentAuthToken(resolve, reject) {
         var cognitoUser = userPool.getCurrentUser();
 
@@ -40,11 +60,11 @@ var WildRydes = window.WildRydes || {};
                 if (err) {
                     reject(err);
                 } else if (!session.isValid()) {
-                    sessionStorage.removeItem('authToken'); // 만료된 세션일 경우 제거
+                    sessionStorage.removeItem('authToken');
                     resolve(null);
                 } else {
                     var token = session.getIdToken().getJwtToken();
-                    sessionStorage.setItem('authToken', token); // 세션 스토리지에 저장
+                    sessionStorage.setItem('authToken', token);
                     resolve(token);
                 }
             });
