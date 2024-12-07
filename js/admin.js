b1519f51eae6aa8628468a0eeb2a5822922d8418
YOON <wodud0747@naver.com>
2024-12-08 04:42:16 +0900

    file upload extension ver1.3

--- js/admin.js
+++ js/admin.js
@@ -39,7 +39,7 @@ fingerprintForm.onsubmit = function (e) {
     formData.append('fingerprint', fingerprintFile3);
 
     // Simulate API Call for Fingerprint Registration
-    fetch('/admin/registerFingerprint', { // Replace with your API endpoint
+    fetch('arn:aws:execute-api:ap-northeast-2:195275678021:tglilj6saa/*/POST/admin/registerFingerprint', { // Replace with your API endpoint
         method: 'POST',
         body: formData,
     })
