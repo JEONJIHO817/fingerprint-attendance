bcc22e1157e10276229c622d80c77d2f516e760f
unknown <jeonjiho817@jbnu.ac.kr>
2024-12-07 20:21:35 +0900

    student

--- js/clockInOut.js
+++ js/clockInOut.js
@@ -17,10 +17,11 @@ WildRydes.clockInOut = WildRydes.clockInOut || {};
         console.error('Error retrieving auth token: ', error);
         window.location.href = '/signin.html';
     });
+
     // 출/퇴근 제출 버튼 클릭 이벤트
     $('#submit-clockinout').click(function() {
-        // 현재 시간 가져오기
-        var currentTime = new Date().toISOString(); // ISO 8601 형식으로 변환
+        // 현재 시간 가져오기 (한국 시간으로 변환)
+        var currentTime = new Date().toLocaleString('ko-KR', { timeZone: 'Asia/Seoul', hour12: false });
         var action = $('#action-select').val(); // Clock In 또는 Clock Out 선택값
         var fileInput = $('#fingerprint-upload')[0];
 
@@ -43,7 +44,7 @@ WildRydes.clockInOut = WildRydes.clockInOut || {};
             data: JSON.stringify(requestData),
             contentType: 'application/json',
             success: function(response) {
-                $('#status-message').text(response.action + '부로 출/퇴근 처리가 완료되었습니다: ');
+                $('#status-message').text(action + ' 처리가 완료되었습니다: ' + currentTime);
             },
             error: function ajaxError(jqXHR, textStatus, errorThrown) {
                 console.error('Error submitting clock-in/out: ', textStatus, ', Details: ', errorThrown);
